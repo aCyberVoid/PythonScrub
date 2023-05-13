598d2b456f172076a02010918e418ecc7924ff1b
aCyberVoid <acybervoid@foxdale.tech>
2023-05-12 21:03:28 -0500

    + added a new folder for new lesson content

--- Projects/fizz_buzz.py
+++ Projects/fizz_buzz.py
@@ -6,11 +6,11 @@
 # Print "FizzBuzz" if the number is divisible by both 3 and 5.
 # Otherwise, print the number.
 for number in range(1, 101):
-  if number % 3 == 0 and number % 5 == 0:
-    print("FizzBuzz")
-  elif number % 3 == 0:
-    print("Fizz")
-  elif number % 5 == 0:
-    print("Buzz")
-  else:
-    print(number)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
