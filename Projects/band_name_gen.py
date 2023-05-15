4009a35ac3e0e66afbe2b2f864fb11a783a3cca4
aCyberVoid <acybervoid@foxdale.tech>
2023-05-15 02:00:58 -0500

    - cleared cache | + updated gitignore files

--- Projects/band_name_gen.py
+++
@@ -1,9 +0,0 @@
-# The print() function is used to display a welcome message to the user.
-print("Welcome to the Band Name Generator.\n")
-# The input() function is used to get the user's city and pet name.
-city = input("What city were you born in?\n")
-pet = input("What was the name of your pet?\n")
-# The + operator is used to concatenate the city and pet name strings to form the band name.
-print("Your band name is " + city +  " " + pet + ".")
-# This is a straightforward and efficient way to combine strings in Python.
-
