7d2964e84e7d578778e35fdb900ac6698e1b29bd
aCyberVoid <void@acybervoid.io>
2023-06-12 00:26:39 -0500

    completed ex39.py

--- PyTech/LearnPython/ex39.py
+++ PyTech/LearnPython/ex39.py
@@ -30,3 +30,25 @@ print("Let's print some states.")
 print("Michigan's abbreviation is: ", states['Michigan'])
 print("Florida's abbreviation is: ", states['Florida'])
 
+# print every state then city dictionary
+print('-' * 10)
+print("Let's print every state then city.")
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+# print every city in state
+print('-' * 10)
+print("Let's print cities for each state")
+for abbrev, city in list(cities.items()):
+    print(f"{state} state is abbreviated {abbrev} and has city {cities[abbrev]}")
+
+# safely get a abbreviation by state that might not be there 
+print('-' * 10)
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas.")
+
+# get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for the state 'TX' is: {city}")
